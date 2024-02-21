22756fecd0daa83b6796da3d0a239a87b0be9c9b
Vadim <vadim.codes@gmail.com>
2024-02-21 00:27:40 -0800

    add TODO

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Save deps: "pip freeze > requirements.txt"
 Install deps: "pip install -r requirements.txt"
 
 run tests: "python3 -m unittest"
+
+#shadow-root
+TODO: chain .shadow_root from top to buttom for all tests to get correct locators (main_page.py, subscription.py, sign_up_page.py)
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,3 +1,4 @@
+from asyncio import sleep
 from fixtures.base import BaseTestCase
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
@@ -5,4 +6,8 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BaseTestCase):
     def click_signup_button(self):
-        self.wait.until(ec.element_to_be_clickable((By.ID("gift-dt-link")))).click()
+        # TODO: chain shadow_root to get access to necessary element(Sign up button) 
+        # moving from top to buttom  
+        # <route-view/> -> <home-element/> -> <home-header/> -> <authstate-context/> -> <signup-button/>
+        # like this: element1.shadow_root.getElementByCS(#).shadow_root (5 levels deep)
+        self.wait.until(ec.element_to_be_clickable((By.XPATH('//*[@aria-label="Sign Up for ABCmouse.com"]')))).click()
